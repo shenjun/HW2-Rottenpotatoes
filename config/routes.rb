970765e3283e97d18654abc19c926fde7b3c26d4
shenjun <shen2004@gmail.com>
2012-06-09 17:29:09 -0700

    init

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rottenpotatoes::Application.routes.draw do
   # first created -> highest priority.
 
   # Sample of regular route:
-  match '/movies/:id' => 'MoviesController#show'
+  # match '/movies/:id' => 'movies#show'
   # Keep in mind you can assign values other than :controller and :action
 
   # Sample of named route:
@@ -49,11 +49,11 @@ Rottenpotatoes::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'MoviesController#index'
+  root :to => 'movies#index'
 
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id(.:format)))'
+  match ':controller/:action'
 end
