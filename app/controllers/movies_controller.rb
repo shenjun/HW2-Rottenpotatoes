2350c9354314058d879c4f6b7e4d6d018e775ce4
shenjun <shen2004@gmail.com>
2012-06-10 17:52:52 -0700

    hw2

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,27 +7,14 @@ class MoviesController < ApplicationController
   end
 
 def index
-	sort = params[:sort] || session[:sort]
+	sort = params[:sort]
     case sort
     when 'title'
       ordering,@title_header = {:order => :title}, 'hilite'
     when 'release_date'
       ordering,@date_header = {:order => :release_date}, 'hilite'
     end
-    @all_ratings = Movie.all_ratings
-    @selected_ratings = params[:ratings] || session[:ratings] || {}
-
-    if params[:sort] != session[:sort]
-      session[:sort] = sort
-      redirect_to :sort => sort, :ratings => @selected_ratings and return
-    end
-
-    if params[:ratings] != session[:ratings] and @selected_ratings != {}
-      session[:sort] = sort
-      session[:ratings] = @selected_ratings
-      redirect_to :sort => sort, :ratings => @selected_ratings and return
-    end
-    @movies = Movie.find_all_by_rating(@selected_ratings.keys, ordering)
+    @movies = Movie.all
   end
 
   def new
